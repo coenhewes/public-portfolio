fdd1921509ee8f5b248d29a4285a421f35a5e7e2
coenhewes <coenhewes@proton.me>
2024-07-29 10:58:15 +1000

    removed contact route, not ready to add

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactForm } from '../_components/contact'
-
-export default function Contact() {
-  return (
-  <div className="flex flex-col justify-center items-center min-h-screen pt-16">
-   <ContactForm /> 
-  </div>
-  )
-}
